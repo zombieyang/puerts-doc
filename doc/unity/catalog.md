a2ab120823b742fb4b019daab206e06cc3a96134
zombieyang(杨浩) <zombieyang@tencent.com>
2022-07-28 19:02:57 +0800

    first commit

---
+++ doc/unity/catalog.md
@@ -0,0 +1,17 @@
+* Introduce to PuerTS
+* Usage
+    * Run JS in Unity
+    * Call C# in JS
+    * Call JS in C#
+* Deep in
+    * JS Inspector
+    * Generate Wrapper
+    * Generate Filter
+    * Blittable Copy
+    * Extention
+    * Other JS Backend
+* Contribute
+    * PuerTS Arch
+    * Build Yourself
+    * UnitTest
+* JS Guide
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './puerts-logo.png';
 import './App.css';
 
 function App() {
@@ -7,18 +7,35 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="App-title">
+          <p><h1>PuerTS</h1><span> - Write your UE/Unity game with TypeScript.</span></p>
+          <span className="subtitle">can be pronounced as pu-erh TS（普洱TS）</span>
+        </section>
+        <section className="App-doc-version">
+          <select name="engine" id="engine">
+            <option value="Unreal">Unreal</option>
+            <option value="Unity">Unity</option>
+          </select>
+          <select name="language" id="language">
+            <option value="English">English</option>
+            <option value="中文">中文</option>
+          </select>
+        </section>
+        <section className="App-link">
+          <a target="_blank" href="https://github.com/tencent/puerts"><img src="https://img.shields.io/github/stars/tencent/puerts?style=social" alt="" /></a>
+          {/* <img src="https://img.shields.io/badge/QQGroup-942696334-blue" alt="" />
+          <img src="https://img.shields.io/badge/QQGroup_UE-689643903-blue" alt="" /> */}
+        </section>
       </header>
+      <article className="App-body">
+        <section className="App-install-bar">
+          <span className="App-install">Install</span>
+        </section>
+        <section className="App-sidebar">
+          <div className="sidebar-content"></div>
+        </section>
+        <section className="App-content"></section>
+      </article>
     </div>
   );
 }
