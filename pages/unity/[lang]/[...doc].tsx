d376236017d76337377bb1f7651c8e44b4170ee8
zombieyang(杨浩) <zombieyang@tencent.com>
2022-08-19 18:05:08 +0800

    recover static

--- pages/unity/[lang]/[...doc].tsx
+++ pages/unity/[lang]/[...doc].tsx
@@ -6,40 +6,40 @@ import { join } from "path";
 
 export default DocPage('unity')
 
-// export async function getStaticProps({ params }) {
-//     if (process.env.NODE_ENV && process.env.NODE_ENV == "development") {
-//         return { props: { params } };
-//     }
+export async function getStaticProps({ params }) {
+    if (process.env.NODE_ENV && process.env.NODE_ENV == "development") {
+        return { props: { params } };
+    }
 
-//     let markdown = '';
-//     try {
-//         markdown = readFileSync(join(process.cwd(), 'public', `/doc/unity/${params.lang}/${params.doc}.md`), 'utf-8');
-//     } catch (e) { }
+    let markdown = '';
+    try {
+        markdown = readFileSync(join(process.cwd(), 'public', `/doc/unity/${params.lang}/${params.doc}.md`), 'utf-8');
+    } catch (e) { }
 
-//     return { props: { markdown, ...params } }
-// }
+    return { props: { markdown, ...params } }
+}
 
-// export async function getStaticPaths() {
-//     if (process.env.NODE_ENV && process.env.NODE_ENV == "development") {
-//         return { paths: [], fallback: true };
-//     }
+export async function getStaticPaths() {
+    if (process.env.NODE_ENV && process.env.NODE_ENV == "development") {
+        return { paths: [], fallback: true };
+    }
 
-//     const paths = [
-//         { params: { lang: 'en', doc: 'install' } },
-//         { params: { lang: 'zhcn', doc: 'install' } }
-//     ];
-//     UnityCatalog.forEach(iter)
-//     function iter(item) {
-//         if (item.md) {
-//             paths.push({ params: { lang: 'en', doc: item.md } })
-//             paths.push({ params: { lang: 'zhcn', doc: item.md } })
-//         }
-//         if (item.child) {
-//             item.child.forEach(iter);
-//         }
-//     }
-//     return {
-//         paths,
-//         fallback: false, // can also be true or 'blocking'
-//     }
-// }
+    const paths = [
+        { params: { lang: 'en', doc: 'install' } },
+        { params: { lang: 'zhcn', doc: 'install' } }
+    ];
+    UnityCatalog.forEach(iter)
+    function iter(item) {
+        if (item.md) {
+            paths.push({ params: { lang: 'en', doc: item.md } })
+            paths.push({ params: { lang: 'zhcn', doc: item.md } })
+        }
+        if (item.child) {
+            item.child.forEach(iter);
+        }
+    }
+    return {
+        paths,
+        fallback: false, // can also be true or 'blocking'
+    }
+}
--- pages/unreal/[lang]/[...doc].tsx
+++ pages/unreal/[lang]/[...doc].tsx
@@ -11,30 +11,30 @@ const fetcher = (url: string) => fetch(url).then((res) => {
   else throw res.statusText;
 })
 
-// export async function getStaticProps({ params }) {
-//     let markdown = '';
-//     try {
-//         markdown = readFileSync(join(process.cwd(), 'public', `/doc/unity/${params.lang}/${params.doc}.md`), 'utf-8');
-//     } catch (e) { }
+export async function getStaticProps({ params }) {
+    let markdown = '';
+    try {
+        markdown = readFileSync(join(process.cwd(), 'public', `/doc/unity/${params.lang}/${params.doc}.md`), 'utf-8');
+    } catch (e) { }
 
-//     return { props: { markdown, ...params } }
-// }
+    return { props: { markdown, ...params } }
+}
 
-// export async function getStaticPaths() {
-//   const paths = [
-//   ];
-//   UnrealCatalog.forEach(iter)
-//   function iter(item) {
-//     if (item.md) {
-//       paths.push({ params: { lang: 'en', doc: item.md } })
-//       paths.push({ params: { lang: 'zhcn', doc: item.md } })
-//     }
-//     if (item.child) {
-//       item.child.forEach(iter);
-//     }
-//   }
-//   return {
-//     paths,
-//     fallback: false, // can also be true or 'blocking'
-//   }
-// }
+export async function getStaticPaths() {
+  const paths = [
+  ];
+  UnrealCatalog.forEach(iter)
+  function iter(item) {
+    if (item.md) {
+      paths.push({ params: { lang: 'en', doc: item.md } })
+      paths.push({ params: { lang: 'zhcn', doc: item.md } })
+    }
+    if (item.child) {
+      item.child.forEach(iter);
+    }
+  }
+  return {
+    paths,
+    fallback: false, // can also be true or 'blocking'
+  }
+}
